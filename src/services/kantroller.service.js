10888ad87a99a85f4d8d21d2eafca6770d003b71
kafkaesc <jared.a.hettinger@gmail.com>
2021-09-13 00:32:04 -0500

    Updating kana data and outlining kantroller

--- src/services/kantroller.service.js
+++ src/services/kantroller.service.js
@@ -1,5 +1,29 @@
 const kantroller = {
+    private hiragana = [],
+    private katakana = [],
+    private romaji = [],
+    private currentKana = '',
 
+    /* Returns the active kana */
+    kana() {},
+
+    /* Go to next kana */
+    next() {},
+
+    /* Go to previous kana */
+    prev() {},
+
+    /* Go to a random kana (excluding the current kana) */
+    random() {},
+
+    /* Return to original kana order */
+    reset () {},
+
+    /* Shuffule the kana order */
+    shuffle() {},
+
+    /* Alternate between katakana and hiragana */
+    toggleKana() {}
 };
 
 export default kantroller;
